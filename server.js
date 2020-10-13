c44bbbf3911e076dec5e2ab78d4008b5f492a148
Garrett Cintron <grrttcintron@gmail.com>
2020-10-12 19:44:35 -0700

    added notes into my work

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'; //necessary for strict mode
 
-require('dotenv').config();
+require('dotenv').config(); //requires the dotenv hidden file
 
 const express = require('express');
 const cors = require('cors');
@@ -8,25 +8,26 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; //opens a port OR port 3000
 
 app.use(cors());
-
+//i need to ask what this does
 app.get('/', (request, response) => {
   response.send();
 });
-
+//gets location data from json with use of functions
 app.get('/location', handleLocation);
+//gets weather data from json with use of functions
 app.get('/weather', weatherConf);
-
+//error message for handling invalid user requests
 function errorMessage(request, response) {
   response.status(500).send('not found');
 }
 
 
 //creates location data
-function Location(geoData) {
-  this.search_query = geoData[0].display_name;
+function Location(geoData, city) {
+  this.search_query = city;
   this.formatted_query = geoData[0].display_name;
   this.latitude = geoData[0].lat;
   this.longitude = geoData[0].lon;
@@ -38,6 +39,20 @@ function Weather(obj2, city) {
   this.time = obj2.valid_date;
   this.city = city;
 }
+//when you get to the internet no neatly packed info just tons of crap data. using constructor function to format the data so it looks nice when it goes back to the user. not the whole response from the internet.
+//handles location data. should also throw error message.
+function handleLocation(request, response) { //this response does not work without a placeholder request
+  try {
+    let geoData = require('./data/location.json');
+    const city = request.query.city;
+    let locationData = new Location(geoData, city); // "seattle" -> localhost:3000/location?city=seattle
+    response.json(locationData);
+  } catch(error) {
+    errorMessage();
+    // response.status(500).send('not found');
+  }
+}//you'll need to loop through data
+
 //handles weather data. should also throw error message
 function weatherConf (request, response) {
   try {
@@ -54,20 +69,7 @@ function weatherConf (request, response) {
     // response.status(500).send('not found');
   }
 }
-//handles location data. should also throw error message.
-function handleLocation(request, response) {
-  try {
-    let geoData = require('./data/location.json');
-    // let skylineData = request.query.geoData;
-    let locationData = new Location(geoData); // "seattle" -> localhost:3000/location?city=seattle
-    response.json(locationData);
-    // response.send(locationData);
-    
-  } catch(error) {
-    errorMessage();
-    // response.status(500).send('not found');
-  }
-}
+
 
 app.get('*', errorMessage)
 
